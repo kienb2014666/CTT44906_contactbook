b43276b341496d97c3805124d265b8821fc54ec7
kienb2014666 <kienb2014666@student.ctu.edu.vn>
2024-03-29 10:20:45 +0700

    Khoi tao du an

---
+++ contactbook-frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+</script>
+
+<template>
+  
+  <h1>Helle, Vue.js</h1>
+</template>
+
+<style scoped>
+.page{
+  max-width: 400px;
+  margin: auto;
+}
+</style>
